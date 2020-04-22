c2f632ac6da301fdf4c6d68a4be43969e12f68dd
Naz Malik <naz@Nazs-MacBook-Pro.local>
2020-04-22 16:11:34 +0100

    seeeeeeds

--- style-changer.js
+++ style-changer.js
@@ -1,4 +1,35 @@
-let labels = document.getElementsByTagName('label')
+let labels = document.getElementsByTagName("label");
+
+let checkboxes = [];
+
 for (let label of labels) {
-  label.style.color = 'pink';
+  const relatedElementId = label.htmlFor;
+  if (relatedElementId === "") continue;
+
+  const relatedElement = document.getElementById(relatedElementId);
+
+  if (relatedElement.type === "checkbox") {
+    checkboxes.push(relatedElement);
+  } else {
+    continue;
+  }
+}
+
+for (let box of checkboxes) {
+  // Hide the checkboxes
+  box.style.position = "absolute";
+  box.style.left = "-9999px";
+  console.log("classes", box.className);
+  var compStyles = window.getComputedStyle(box);
+  var boxMargin = compStyles.getPropertyValue("margin");
+
+  // Insert new pic
+  var imgURL = chrome.extension.getURL("images/seed-emoji.png");
+
+  var newNode = document.createElement("img");
+  newNode.src = imgURL;
+  newNode.style.height = "20px";
+  newNode.style.width = "20px";
+  newNode.style.setProperty("margin", boxMargin);
+  box.after(newNode);
 }
