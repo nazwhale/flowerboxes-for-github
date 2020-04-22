1cdd6b39dbcef80befb9864f10ae8de8b2d34428
niharikak101 <niharikak101@gmail.com>
2020-04-22 15:23:19 +0100

    change label color

--- popup.js
+++ popup.js
@@ -1,15 +1,17 @@
 let changeColor = document.getElementById("changeColor");
 
-chrome.storage.sync.get("color", function(data) {
+chrome.storage.sync.get("color", function (data) {
   changeColor.style.backgroundColor = data.color;
   changeColor.setAttribute("value", data.color);
 });
 
-changeColor.onclick = function(element) {
+changeColor.onclick = function (element) {
   let color = element.target.value;
-  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-    chrome.tabs.executeScript(tabs[0].id, {
-      code: 'document.body.style.backgroundColor = "' + color + '";'
+
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    chrome.tabs.executeScript({
+      file: "/style-changer.js",
+      allFrames: true
     });
   });
 };
---
+++ style-changer.js
@@ -0,0 +1,4 @@
+let labels = document.getElementsByTagName('label')
+for (let label of labels) {
+  label.style.color = 'pink';
+}
