ac547687222401dd555992df24e75daacd5234ad
niharikak101 <niharikak101@gmail.com>
2020-04-22 16:54:45 +0100

    toggle checkboxes

--- style-changer.js
+++ style-changer.js
@@ -2,6 +2,11 @@ let labels = document.getElementsByTagName("label");
 
 let checkboxes = [];
 
+let nodeBoxes = [];
+
+var clickedImgURL = chrome.extension.getURL("images/cherry-blossom-emoji.png");
+var imgURL = chrome.extension.getURL("images/seed-emoji.png");
+
 for (let label of labels) {
   const relatedElementId = label.htmlFor;
   if (relatedElementId === "") continue;
@@ -19,17 +24,30 @@ for (let box of checkboxes) {
   // Hide the checkboxes
   box.style.position = "absolute";
   box.style.left = "-9999px";
-  console.log("classes", box.className);
+
   var compStyles = window.getComputedStyle(box);
   var boxMargin = compStyles.getPropertyValue("margin");
 
-  // Insert new pic
-  var imgURL = chrome.extension.getURL("images/seed-emoji.png");
 
   var newNode = document.createElement("img");
+
   newNode.src = imgURL;
   newNode.style.height = "20px";
   newNode.style.width = "20px";
   newNode.style.setProperty("margin", boxMargin);
+  nodeBoxes.push({ newNode, box });
+
   box.after(newNode);
+}
+
+for (let nodeBox of nodeBoxes) {
+  let node = nodeBox.newNode;
+  let box = nodeBox.box
+  node.onclick = function () {
+    box.checked = !box.checked
+    if (box.checked) node.src = clickedImgURL;
+    else node.src = imgURL;
+
+    console.log({ box })
+  }
 }
