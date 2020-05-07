267f29deabe4cb39871d4965a419a840bbbafdef
Naz Malik <naz@Nazs-MacBook-Pro.local>
2020-05-07 10:44:29 +0100

    remove options, update logo to seed

--- background.js
+++ background.js
@@ -6,7 +6,11 @@ chrome.runtime.onInstalled.addListener(function() {
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
     chrome.declarativeContent.onPageChanged.addRules([
       {
-        conditions: [new chrome.declarativeContent.PageStateMatcher({})],
+        conditions: [
+          new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: { hostEquals: "github.com" }
+          })
+        ],
         actions: [new chrome.declarativeContent.ShowPageAction()]
       }
     ]);
